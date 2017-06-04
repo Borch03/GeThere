457f0111fdae794498aee68d5a00d540b0629a88
Borch03 <dburdzy@gmail.com>
2017-06-04 17:26:37 +0200

    ViewMap Activity has been changed. Currently, it shows a user his location.

--- GeThere/src/main/java/pl/edu/agh/gethere/controller/ViewMapActivity.java
+++ GeThere/src/main/java/pl/edu/agh/gethere/controller/ViewMapActivity.java
@@ -1,18 +1,59 @@
 package pl.edu.agh.gethere.controller;
 
-import android.content.Intent;
+import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import pl.edu.agh.gethere.R;
+import pl.edu.agh.gethere.connection.LocationProvider;
+import pl.edu.agh.gethere.model.Coordinates;
 
-public class ViewMapActivity extends FragmentActivity {
+public class ViewMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleMap mMap;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, NavigationActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        BitmapDescriptor icon = BitmapDescriptorFactory.fromBitmap(
+                BitmapFactory.decodeResource(context.getResources(), R.drawable.google_dot));
+
+        LocationProvider locationProvider = new LocationProvider();
+        LatLng location = convertCoordinatesToLatLng(locationProvider.getLocation(context));
+        mMap.addMarker(new MarkerOptions().position(location)).setIcon(icon);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 17f));
+    }
+
+    private LatLng convertCoordinatesToLatLng(Coordinates coordinates) {
+        return new LatLng(coordinates.getLatitude(), coordinates.getLongitude());
     }
 }
